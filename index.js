4fb8552ec96635e54c5a081bcfba7f512d6e0f49
wendy yu <yu.wendyw@gmail.com>
2018-03-01 02:45:11 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay (musicians, instruments) {
+  var array = [];
+  for (let i = 0; i > musicians.length; i++) {
+    array.push(musicians + "plays" + instruments);
+  }
+  return array
+}
+
+function johnLennonFacts (facts) {
+  var array = [];
+  var i = 0;
+  while (i < facts.length) {
+    array.push(facts[i] + "!!!");
+    i++;
+  }
+return array
+}
+
+function iLoveTheBeatles () {
+  do {
+    return 'I love the Beatles!';
+    number++;
+  } while (i === 17)
+  return 'I love the Beatles!'
+}
